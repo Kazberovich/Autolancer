e143f22bc9e3f205983c2e2f29547513606813bb
Kazberovich <kazbers@gmail.com>
2014-06-18 17:21:16 +0300

    MapViewController added. pod MapBox added

---
+++ Autolancer/Autolancer/MapViewController/KSMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  KSMapViewController.h
+//  Autolancer
+//
+//  Created by mac-214 on 18.06.14.
+//  Copyright (c) 2014 mac-214. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSMapViewController : UIViewController
+
+@end
