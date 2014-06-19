b671f5e0b7e9c5831d03cc4dc961c6065cfc09c2
Kazberovich <kazbers@gmail.com>
2014-06-20 00:50:06 +0300

    subscribes

---
+++ Autolancer/Autolancer/SubscribesViewController/KSSubscribesViewController.h
@@ -0,0 +1,26 @@
+//
+//  KSSubscribesViewController.h
+//  Autolancer
+//
+//  Created by user on 14.06.14.
+//  Copyright (c) 2014 mac-214. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <LPPopupListView/LPPopupListView.h>
+#import <LPPopupListViewCell.h>
+
+@interface KSSubscribesViewController : UIViewController<LPPopupListViewDelegate>
+
+@property (nonatomic, retain) IBOutlet UIScrollView *scrollView;
+
+@property (nonatomic, retain) IBOutlet UIButton *selectTenderType;
+@property (nonatomic, retain) IBOutlet UIButton *selectCatrgory;
+@property (nonatomic, retain) IBOutlet UIButton *selectPlace;
+@property (nonatomic, retain) IBOutlet UITextView *typeTextView;
+@property (nonatomic, retain) IBOutlet UITextView *categoryTextView;
+@property (nonatomic, retain) IBOutlet UITextView *placeTextView;
+
+- (IBAction)buttonClicked:(id)selector;
+
+@end
